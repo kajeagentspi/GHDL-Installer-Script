c4c392beadaa42618216d73f683180a603a7c8c1
Jake Tagnepis <jptagnepis@up.edu.ph>
2018-02-07 17:42:17 +0800

    Create ghdl_install.sh

---
+++ ghdl_install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Commands from
+# https://theintobooks.wordpress.com/2016/08/24/installing-ghdl-from-source-on-ubuntu-16-04/
+# change permissions first
+# chmod a+x ghdl_install.sh
+# then run with sudo
+# sudo bash ghdl_install.sh
+set -e
+git clone git://git.code.sf.net/p/ghdl-updates/ghdl-updates.git
+cd ghdl-updates
+git branch
+git checkout ghdl-0.33
+apt install gnat gtkwave m4 -y
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2
+tar xvjf gcc-4.9.4.tar.bz2
+./configure --with-gcc=gcc-4.9.4/
+make copy-sources
+
+# install gmp
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-4.3.2.tar.bz2
+tar xvjf gmp-4.3.2.tar.bz2
+mkdir gmp-4.3.2/gmp-objs/
+cd gmp-4.3.2/gmp-objs/
+../configure --prefix=/usr/local --disable-shared
+make
+make install
+cd ../..
+
+# install mpfr
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-2.4.2.tar.bz2
+tar xvjf mpfr-2.4.2.tar.bz2
+mkdir mpfr-2.4.2/mpfr-objs/
+cd mpfr-2.4.2/mpfr-objs/
+../configure --prefix=/usr/local --disable-shared --with-gmp=/usr/local
+make
+make install
+cd ../..
+
+# install mpc
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz
+tar xvzf mpc-0.8.1.tar.gz
+mkdir mpc-0.8.1/mpc-objs/
+cd mpc-0.8.1/mpc-objs/
+../configure --prefix=/usr/local --disable-shared --with-gmp=/usr/local
+make
+make install
+cd ../..
+
+# Install 
+cd gcc-4.9.4
+mkdir gcc-objs
+cd gcc-objs/
+../configure --prefix=/opt/ghdl-updates --enable-languages=c,vhdl --disable-bootstrap --with-gmp=/usr/local --disable-lto --disable-multilib
+PATH=/usr/lib/gcc/x86_64-linux-gnu/4.9:$PATH make
+PATH=/usr/lib/gcc/x86_64-linux-gnu/4.9:$PATH make install MAKEINFO=true
+PATH=$PATH:/opt/ghdl-updates/bin
+PATH=/opt/ghdl-updates/bin:$PATH
+
+# Cleanup
+rm -rf ghdl-updates
